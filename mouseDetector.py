8cdcbd16d48f66c5cbe1792f62cd27d314aef805
PrincessMagnificent <axio.axiomatic@gmail.com>
2016-03-14 19:45:39 +0100

    graphic representation of where you clicked, red static on last two clicks

--- mouseDetector.py
+++ mouseDetector.py
@@ -2,11 +2,13 @@
 
 import random, pygame, sys
 from pygame.locals import *
+import pygame.gfxdraw
 
 FPS = 30 # frames per second, the general speed of the program
 WINDOWWIDTH = 640 # size of window's width in pixels
 WINDOWHEIGHT = 480 # size of windows' height in pixels
 BOXSIZE = 40 # size of box height & width in pixels
+skitting = 10
 
 #            R    G    B
 GRAY = (100, 100, 100)
@@ -21,6 +23,8 @@ BLACK = (0,0,0)
 BACKGROUND = BLACK
 TARGET = RED
 SEEKER = GREEN
+def skitterDot(x):
+    return x+random.randint(-skitting,skitting)
 
 def main():
     global FPSCLOCK, DISPLAYSURF, BACKGROUND
@@ -30,12 +34,19 @@ def main():
     
     mousex = -1
     mousey = -1
+    lastmousex = 100
+    lastmousey = 100
     
     pygame.display.set_caption("MOUSE DETECTER")
     
     
     while True:
         DISPLAYSURF.fill(BACKGROUND)
+        pygame.draw.line(DISPLAYSURF, WHITE, (lastmousex,lastmousey), (mousex,mousey))
+        ##line(Surface, color, start_pos, end_pos, width=1) -> Rect
+        pygame.gfxdraw.pixel(DISPLAYSURF, skitterDot(mousex), skitterDot(mousey), RED)
+        pygame.gfxdraw.pixel(DISPLAYSURF, skitterDot(lastmousex), skitterDot(lastmousey), RED)
+        ##For a joke, added two jumping red dots at either end of the drawn line
         
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
@@ -46,8 +57,12 @@ def main():
             elif event.type == KEYUP:
                 BACKGROUND = BLACK
             elif event.type == MOUSEBUTTONUP:
+                lastmousex = mousex
+                lastmousey = mousey
                 mousex, mousey = event.pos
                 print("X: %i  Y: %i"%(mousex,mousey))
+                
+                
                
         pygame.display.update()
         FPSCLOCK.tick(FPS)    
